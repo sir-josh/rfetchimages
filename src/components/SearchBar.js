aa594ccfeb6892afa11a42c7168d4544d17c14e7
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-04-20 13:20:14 +0100

    add:handle event on input change

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    onInputChange(event){
+        console.log(event.target.value);
+    }
 
     render(){
         return (
