f5cd8ad6d399036524c5ba17defd13f563d0868e
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-04-20 13:10:47 +0100

    add some css style to searchbar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const SearchBar = () => {
   return (
-    <div>
-        <form>
-            <input type="text" />
+    <div className='ui segment'>
+        <form className='ui form'>
+            <div className='field'>
+                <label>Image Search</label>
+                <input type="text" />
+            </div>
         </form>
     </div>
   )
