78706258ed82062a9b76e9f48ce7572902285e99
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-04-23 03:35:46 +0100

    refactor: add imageRef

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 
 export class ImageCard extends Component {
+    constructor(props){
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount(){
+        this.imageRef.current.addEventListener('load', this.setGridSpan);
+    }
+
+    setGridSpan = () => {
+        console.log(this.imageRef.current.clientHeight);
+    }
 
     render() {
         const { description, urls:{regular}} = this.props.image;
 
         return (
-         <img alt={description} src={regular} />
+         <img alt={description} src={regular} ref={this.imageRef} />
         )
     }
 }
